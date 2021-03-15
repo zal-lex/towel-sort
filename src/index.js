ad7589e2b44f38a7cfffa314754620f129795704
Alexey Zalyotov <alexey.zalyotov@rubyroidlabs.com>
2021-03-15 22:13:29 +0300

    feat: implement towel sort algorithm

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) { return [] };
+  let result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    i % 2 === 0 ? result = result.concat(matrix[i]) :result = result.concat(matrix[i].reverse());
+  }
+  return result;
 }
